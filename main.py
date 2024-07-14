b2ff9a0d02237b2b7c1c8e3a59bf67258d56a8de
debankanmitra <debankanmitra@gmail.com>
2024-07-14 13:27:10 +0530

    aws textract implementation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, UploadFile, File
 import uvicorn
 
+from textract import detect_file_text
+
 
 app = FastAPI()
 
@@ -12,7 +14,8 @@ async def test():
 
 @app.post("/upload")
 def recommnedation(image: UploadFile= File(...)):
-    return {"filename": image.filename}
+    response = detect_file_text(image)
+    return {"response": response}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
--- textract.py
+++ textract.py
@@ -1,3 +1,33 @@
 import boto3
+from fastapi import UploadFile
+import logging
+from botocore.exceptions import ClientError
 
-client = boto3.client('textract')
+logger = logging.getLogger(__name__)
+
+def detect_file_text(document_file: UploadFile):
+        textract_client = boto3.client('textract')
+        """
+        Detects text elements in a local image file or from in-memory byte data.
+        The image must be in PNG or JPG format.
+
+        :param document_file_name: The name of a document image file.
+        :param document_bytes: In-memory byte data of a document image.
+        :return: The response from Amazon Textract, including a list of blocks
+                 that describe elements detected in the image.
+        """
+        if document_file is not None:
+                document_bytes = document_file.file.read()
+
+        response = textract_client.detect_document_text(
+            Document={"Bytes": document_bytes}
+        )
+        # Loop through blocks and extract text
+        text = ""
+        blocks = response['Blocks']
+        for block in blocks:
+            if block['BlockType'] == "LINE":
+                text += block["Text"] + "\n"  # Add newline for each line
+
+        return text
+
