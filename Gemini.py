7bf07e41d47afedb55c06a4fc4e4d0d8b95b0c35
debankanmitra <debankanmitra@gmail.com>
2024-07-14 13:43:30 +0530

    genai llm resposne

---
+++ Gemini.py
@@ -0,0 +1,30 @@
+import os
+import google.generativeai as genai
+
+
+def create_prompt(ingredients, health_history):
+    prompt = f"""
+    Given the following ingredients and the user's health history, determine if the product is suitable for the user.
+
+    Ingredients: {ingredients}
+
+    Health History: {health_history}
+
+    Please respond with:
+    1. Whether the user should buy the product or not.
+    2. If the user should not buy the product, explain why by identifying any problematic ingredients and their potential effects on the user's health.
+
+    Note: If the user's query goes beyond the context of ingredients and health history, respond with "This question is beyond the context of the provided information. Please provide ingredient information and health history for an appropriate response."
+    """
+    return prompt
+
+def Llm_result(ingredients,health_history):
+    genai.configure(api_key=os.environ.get("GENAI_API_KEY"))
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    prompt = create_prompt(ingredients, health_history)
+    response = model.generate_content(prompt)
+
+    return response.text
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile, File
 import uvicorn
 
+from Gemini import Llm_result
 from textract import detect_file_text
 
 
@@ -14,8 +15,10 @@ async def test():
 
 @app.post("/upload")
 def recommnedation(image: UploadFile= File(...)):
-    response = detect_file_text(image)
-    return {"response": response}
+    ingredients = detect_file_text(image)
+    health_history = "Diabetes, Gluten intolerance"
+    result = Llm_result(ingredients, health_history)
+    return {"response": result}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
